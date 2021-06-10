6c0e339e79624357d309c3b8ee7491fa9b5843d8
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-06-10 18:39:56 +0700

    update appcontroller

--- src/main/java/com/example/comlakecrawler/controller/AppController.java
+++ src/main/java/com/example/comlakecrawler/controller/AppController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AppController {
-    @GetMapping("")
+    @GetMapping(value = "")
     public String viewHomePage(){
         return "home";
     }
+
+    
 }
