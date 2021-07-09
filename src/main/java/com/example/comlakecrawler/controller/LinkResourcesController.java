385706cbb18edc846a33a3ce36d11f6b605d8981
JeffKi11er <45094606+JeffKi11er@users.noreply.github.com>
2021-07-10 01:09:29 +0700

    fix paging successfully

--- src/main/java/com/example/comlakecrawler/controller/LinkResourcesController.java
+++ src/main/java/com/example/comlakecrawler/controller/LinkResourcesController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -42,8 +43,24 @@ public class LinkResourcesController {
     @RestController
     public class SourcesRestSearchController {
         @GetMapping("/results")
-        public List<LinkResources> findAllWithSearch(@RequestParam Optional<String> name){
-            return sourcesService.findByName(name.orElse("_"));
+        public List<LinkResources> findAllWithSearch(@RequestParam Optional<String> name,@RequestParam Optional<Integer>pageNumber){
+            List<LinkResources>overallResults = sourcesService.findByName(name.orElse("_"));
+            List<ArrayList<LinkResources>>theBigPage = new ArrayList<>();
+            int i = 0;
+            int count = 0;
+            ArrayList<LinkResources>theSmallerOne = new ArrayList<>();
+            do {
+                if(count<6){
+                     count++;
+                }else {
+                    count = 0;
+                    theBigPage.add(theSmallerOne);
+                    theSmallerOne = new ArrayList<>();
+                }
+                theSmallerOne.add(overallResults.get(i));
+                i++;
+            }while (i<overallResults.size());
+            return theBigPage.get(pageNumber.orElse(0));
         }
     }
 
--- src/main/java/com/example/comlakecrawler/repository/SourcesRepository.java
+++ src/main/java/com/example/comlakecrawler/repository/SourcesRepository.java
@@ -2,6 +2,7 @@ package com.example.comlakecrawler.repository;
 
 import com.example.comlakecrawler.utils.LinkResources;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
